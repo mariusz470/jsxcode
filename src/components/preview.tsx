8362ca9117419e97d3650a2488e5e670f133601a
Mariusz Kiciński <mariusz470@users.noreply.github.com>
2021-07-04 17:38:43 +0200

    Added resizable components and code execution

---
+++ src/components/preview.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useRef } from "react";
+import "./preview.css";
+
+interface PreviewProps {
+  code: string;
+}
+
+const html = `
+<html>
+  <head>
+    <style>html { background-color: #fff} </style>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script>
+      window.addEventListener('message', (event) => {
+        try {
+          eval(event.data)
+        } catch (err) {
+          const root = document.querySelector('#root');
+          root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+          console.error(err);
+        }
+      }, false)
+    </script>
+  </body>
+</html>
+`;
+
+const Preview: React.FC<PreviewProps> = ({ code }) => {
+  const iframe = useRef<any>();
+
+  useEffect(() => {
+    iframe.current.srcdoc = html;
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, "*");
+    }, 50);
+  }, [code]);
+  return (
+    <div className="preview-wrapper">
+      <iframe
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+        title="preview"
+      />
+    </div>
+  );
+};
+
+export default Preview;
